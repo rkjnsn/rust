44cf02b503f326ad34fb60a304c111634ff1d299
Brian Anderson <banderson@mozilla.com>
2014-01-31 17:21:54 -0800

    std: Move swap and replace to prim

--- src/libprim/lib.rs
+++ src/libprim/lib.rs
@@ -35,3 +35,59 @@ pub mod intrinsics;
 #[cfg(not(test))] pub mod kinds;
 pub mod mem;
 pub mod ptr;
+
+/**
+ * Swap the values at two mutable locations of the same type, without
+ * deinitialising or copying either one.
+ */
+#[inline]
+pub fn swap<T>(x: &mut T, y: &mut T) {
+    unsafe {
+        // Give ourselves some scratch space to work with
+        let mut tmp: T = intrinsics::uninit();
+        let t: *mut T = &mut tmp;
+
+        // Perform the swap, `&mut` pointers never alias
+        let x_raw: *mut T = x;
+        let y_raw: *mut T = y;
+        ptr::copy_nonoverlapping_memory(t, &*x_raw, 1);
+        ptr::copy_nonoverlapping_memory(x, &*y_raw, 1);
+        ptr::copy_nonoverlapping_memory(y, &*t, 1);
+
+        // y and t now point to the same thing, but we need to completely forget `tmp`
+        // because it's no longer relevant.
+        cast::forget(tmp);
+    }
+}
+
+/**
+ * Replace the value at a mutable location with a new one, returning the old
+ * value, without deinitialising or copying either one.
+ */
+#[inline]
+pub fn replace<T>(dest: &mut T, mut src: T) -> T {
+    swap(dest, &mut src);
+    src
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{swap, replace};
+
+    #[test]
+    fn test_swap() {
+        let mut x = 31337;
+        let mut y = 42;
+        swap(&mut x, &mut y);
+        assert_eq!(x, 42);
+        assert_eq!(y, 31337);
+    }
+
+    #[test]
+    fn test_replace() {
+        let mut x = 1;
+        let y = replace(&mut x, 2);
+        assert!(x == 2);
+        assert!(y == 1);
+    }
+}
