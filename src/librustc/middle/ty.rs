815f26c6b418648116bda486bd4d8dd68277f688
Jared Roesch <roeschinc@gmail.com>
2015-07-16 11:26:02 -0700

    Fix error message spans

--- src/librustc/middle/ty.rs
+++ src/librustc/middle/ty.rs
@@ -114,8 +114,6 @@ pub struct Field<'tcx> {
     pub mt: TypeAndMut<'tcx>
 }
 
-
-
 // Enum information
 #[derive(Clone)]
 pub struct VariantInfo<'tcx> {
@@ -2199,6 +2197,7 @@ pub struct TypeParameterDef<'tcx> {
     pub def_id: ast::DefId,
     pub space: subst::ParamSpace,
     pub index: u32,
+    pub default_def_id: DefId, // for use in error reporing about defaults
     pub default: Option<Ty<'tcx>>,
     pub object_lifetime_default: ObjectLifetimeDefault,
 }
@@ -5102,7 +5101,7 @@ impl<'tcx> fmt::Display for TypeError<'tcx> {
                        values.found)
             },
             TyParamDefaultMismatch(ref values) => {
-                write!(f, "conflicting type parameter defaults {} and {}",
+                write!(f, "conflicting type parameter defaults `{}` and `{}`",
                        values.expected.ty,
                        values.found.ty)
             }
@@ -5471,11 +5470,11 @@ impl<'tcx> ctxt<'tcx> {
                              expected.ty,
                              found.ty));
                 self.sess.span_note(expected.definition_span,
-                    &format!("...a default was defined"));
+                    &format!("a default was defined here..."));
                 self.sess.span_note(expected.origin_span,
                     &format!("...that was applied to an unconstrained type variable here"));
                 self.sess.span_note(found.definition_span,
-                    &format!("...a second default was defined"));
+                    &format!("a second default was defined here..."));
                 self.sess.span_note(found.origin_span,
                     &format!("...that also applies to the same type variable here"));
             }
